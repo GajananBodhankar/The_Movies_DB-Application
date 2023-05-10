8757ebf893727d0e2fc6841efd68825cb22a5801
Gajanan Bodhankar <124874678+GajananBodhankar@users.noreply.github.com>
2023-05-10 20:09:12 +0530

    Delete TheMovieDB directory

--- TheMovieDB/Redux/MovieStore.tsx
+++
@@ -1,7 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit';
-import {MovieReducer} from './MovieSlice';
-export const MovieStore = configureStore({
-  reducer: {MovieReducer},
-});
-export type RootState = ReturnType<typeof MovieStore.getState>;
-export type AppDispatch = typeof MovieStore.dispatch;
--- TheMovieDB/config.tsx
+++
@@ -1,3 +0,0 @@
-export const API_KEY='4f9ebc08e6c1e549275e0a510bcdb7f9';
-export const BASE_URL='https://api.themoviedb.org/3';
-export const IMAGE_URL='https://image.tmdb.org/t/p/original'
